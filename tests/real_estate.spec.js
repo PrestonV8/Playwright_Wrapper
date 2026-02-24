64bee1b563e7e63e376cb9bf72b84f6d15e3beef
PrestonV <PrestonViado@outlook.com>
2026-02-24 13:19:57 -0500

    Removed the "headless" argument in the browser variable in the playwright test

--- tests/real_estate.spec.js
+++ tests/real_estate.spec.js
@@ -5,9 +5,7 @@ const { chromium } = require('@playwright/test');
 test.use({storageState: 'auth.json'});
 
 test('real estate test', async () => {
-  const browser = await chromium.launch({
-    headless: false
-  });
+  const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://larizzare.com/');
