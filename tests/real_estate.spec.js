fb162f6a718e46dfa9a9b7079fb8121d61ac7a77
PrestonV <PrestonViado@outlook.com>
2026-02-24 13:03:29 -0500

    Created a new test that visits a realtor's website, also skips the captcha

---
+++ tests/real_estate.spec.js
@@ -0,0 +1,20 @@
+const {test, expect} = require('@playwright/test');
+const {chromium} = require('@playwright/test');
+
+// uses authenticated storage state to skip the catcha before running the test
+test.use({storageState: 'auth.json'});
+
+test('real estate test', async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://larizzare.com/');
+  await page.getByRole('link', { name: 'Services' }).click();
+  await page.getByRole('link', { name: 'Contact' }).nth(1).click();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+});
